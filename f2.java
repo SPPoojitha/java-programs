8962477b3b19e009feca4394392589ee8f7642f7
Poojitha Prakash <97533448+SPPoojitha@users.noreply.github.com>
2023-12-11 15:19:29 +0530

    Create f2.java

---
+++ f2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        
+        scanner.close();
+    }
+
+    // Function to check whether a number is prime or not
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
